c7a4f0a5967d04e14274ccee0cc43970353e5ff0
LJ Stevenson <StevensonLucasJ@JohnDeere.com>
2020-11-12 15:33:38 -0600

    trying again

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const corsOptions = {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
