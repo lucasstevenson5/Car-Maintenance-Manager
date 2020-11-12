bf38bd7fc5d3dbc93163d5022c2ada4c94f5482c
LJ Stevenson <StevensonLucasJ@JohnDeere.com>
2020-11-12 15:30:28 -0600

    trying cors stuff

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const corsOptions = {
     optionsSuccessStatus: 200 //legacy browsers
 }
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
