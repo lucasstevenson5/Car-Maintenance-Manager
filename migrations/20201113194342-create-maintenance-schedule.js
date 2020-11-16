9169dda4bfcfe18ef905e46f76ba4234aef49f84
LJ Stevenson <StevensonLucasJ@JohnDeere.com>
2020-11-16 16:04:16 -0600

    updated maintenance schedule for deployed version

--- migrations/20201113194342-create-maintenance-schedule.js
+++ migrations/20201113194342-create-maintenance-schedule.js
@@ -1,30 +1,30 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('MaintenanceSchedules', {
+    await queryInterface.createTable('maintenance_schedules', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      itemDescription: {
+      item_description: {
         allowNull: false,
         type: Sequelize.STRING
       },
-      carMiles: {
+      car_miles: {
         allowNull: false,
         type: Sequelize.INTEGER
       },
       notes: {
         type: Sequelize.STRING
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         defaultValue: new Date(),
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         defaultValue: new Date(),
         type: Sequelize.DATE
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('MaintenanceSchedules');
+    await queryInterface.dropTable('maintenance_schedules');
   }
 };
